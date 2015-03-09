f5ebbc370843f7e9fcb5cb2b8cd104d85dbc0087
Thomas Scholz <thomas.scholz@me.com>
2015-03-09 12:56:29 +0100

    try to select active products only

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ActiveRecord::Base
   has_many :variants, -> {order :price}
 
   validates :title, presence: true
+
+  # scope :active, -> { includes(:variants).where(variants: {is_active: true}) }
+  # cant't find the error
 end
--- app/models/variant.rb
+++ app/models/variant.rb
@@ -2,4 +2,6 @@ class Variant < ActiveRecord::Base
   belongs_to :product
 
   validates :price, :quantity, presence: true
+
+  # scope :active, ->{ is_active: true }
 end
